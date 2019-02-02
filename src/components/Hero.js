56e2308745e7158c4c287831c62859146897957c
tanner.lovell <tanner.lovell@banno.com>
2019-02-02 10:52:22 -0600

    Initial commit of files.

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from "react"
+import styled from "styled-components"
+
+const HeroWrapper = styled.div`
+  grid-row-start: hero;
+  grid-row-end: about;
+`
+
+const HeroImg = styled.img`
+  
+`
+
+export default () => (
+
+  <HeroWrapper />
+
+)
---
+++ src/components/PageWrapper.js
@@ -0,0 +1,22 @@
+import React from "react"
+import styled from "styled-components"
+
+//Set up grid for page below. Preferrably Mobile-First.
+const PageWrapper = styled.div`
+  display: grid;
+  grid-template-areas:
+    "hero"
+    "about"
+    "cutomers"
+    "brands"
+    "deals"
+    "coupons"
+    "storeInfo"
+    "map";
+  
+`
+
+export default () => (
+
+
+)
